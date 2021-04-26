2c04a6b82e2dd8e1239f96ecbdc6def7e0661431
schw240 <schw240@gmail.com>
2021-04-26 22:37:05 +0900

    올리는 버튼 추가

--- project15/js/script.js
+++ project15/js/script.js
@@ -4,18 +4,37 @@ function scrollHandler() {
   // 모든 playlist를 하나씩 살펴보고
   // 그 playlist의 중간 지점 좌표가 windowBottom보다 작으면
   // 그 playlist를 보이게 하기
-  let playlists = $('.playlist');
-  for (let i = 0; i < playlists.length; i++) {
-    let playlist = $(playlists[i])
-    let playlistHalf = playlist.position().top + playlist.outerHeight()
+  // let playlists = $('.playlist');
+  // for (let i = 0; i < playlists.length; i++) {
+  //   let playlist = $(playlists[i])
+  //   let playlistHalf = playlist.position().top + playlist.outerHeight() / 2;
+    
+  //   if (playlistHalf < windowBottom) {
+  //     playlist.animate({'opacity': '1'}, 1500);
+  //   }
+  // }
+  $('.playlist').each(function () {
+    let playlist = $(this);
+    let playlistHalf = playlist.position().top + playlist.outerHeight() / 2;
     
     if (playlistHalf < windowBottom) {
-      playlist.animate({'opacit': '1'}, 1500);
+      playlist.animate({'opacity': '1'}, 1500);
     }
+  });
+  
+  if (windowBottom == $(document).height()) {
+    $('.to-top-btn').fadeIn();
+  } else {
+    $('to-top-btn').fadeOut();
   }
+  
 }
 
 $(window).on('scroll', scrollHandler);
 
 // 처음 로딩될 때 호출
 scrollHandler();
+
+$('.to-top-btn').on('click', function() {
+  $('html, body').animate({scrollTop: 0}, 1000);
+});
