fd1a0d6970efcafa14848a4e6e44a6b27e9b717a
schw240 <schw240@gmail.com>
2021-05-15 20:28:32 +0900

    블로그 Header만들기 연습

---
+++ HTML_study/js/main.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+let input = fs.readFileSync('/dev/stdin').toString().split('\n')
+
+let inputs = []
+inputs = input[0].split(' ')
+
+const N = Number(input[0])
+const X = Number(input[1])
+
+let numbers = []
+numbers = input[1].split(' ')
+
+let res = ''
+for (let i = 0; i < N; i++) {
+    if (numbers[i] < X) {
+        res += numbers[i] + ' '
+    }
+}
+
+console.log(res)
